0881a699cde3a3ae9a648367be6e17833452897c
samuelpd22 <samuel050101@hotmail.com>
2024-10-22 20:00:55 -0300

    feat: componentes basicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrincpalComponent } from "./components/princpal/princpal.component";
+import { NavbarComponent } from "./components/layout/navbar/navbar.component";
+import { FooterComponent } from "./components/layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrincpalComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
